99c8651fbdf027f039f8a9aafabc32d3808c9448
Harley Mamalias <harleymamalias@gmail.com>
2025-01-17 00:36:56 +0800

    converted component/files into typescript

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-import styled from "styled-components";
-import PokedexPage from "./pages/PokedexPage";
-
-function App() {
-  return <PokedexPage />;
-}
-
-export default App;
--- src/utils/capitalizeFirstLetter.js
+++
@@ -1,3 +0,0 @@
-export const capitalFirstLetter = (string) => {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-}
